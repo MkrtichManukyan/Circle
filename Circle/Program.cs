d55a3f0397fedbbfa5962f4d68145dc56f29e597
Mkrtich Manukyan <mkrtichmanukyan1@gmail.com>
2023-06-30 15:11:54 +0400

    Add project files.

---
+++ Circle/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int cricliY = 10;
+
+for (int y = -cricliY; y <= cricliY; y++)
+{
+	for (int x = -cricliY; x <= cricliY; x++)
+	{
+        double distance = Math.Sqrt(x * x + y * y);
+        if (distance <= cricliY)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
